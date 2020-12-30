321610c32b94da0c0328bf3fb3e612ee33507219
Ghulam Rasool <ghulam.appelit@nextbridge.com>
2020-12-30 20:43:29 +0500

    added packages

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,13 +1,13 @@
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 import rootReducer from '../reducers/index';
 import {persistStore, persistReducer} from 'redux-persist';
 // import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
-// import AsyncStorage from '@react-native-community/async-storage'
-import FSStorage from 'redux-persist-fs-storage';
+import AsyncStorage from '@react-native-community/async-storage';
+import thunk from 'redux-thunk';
 
 const persistConfig = {
   key: 'root',
-  storage: FSStorage(),
+  storage: AsyncStorage,
   whitelist: ['auth'],
 };
 
@@ -16,6 +16,7 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 export default () => {
   let store = createStore(
     persistedReducer,
+    applyMiddleware(thunk),
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
       window.__REDUX_DEVTOOLS_EXTENSION__(),
   );
